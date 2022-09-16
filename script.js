ad92760e0eb766598e39c05a7eef08552b1d7a1c
Lorenzo_Bavastro <lorenzo.bavastro@gmail.com>
2022-09-16 14:43:32 +0200

    Aggiunta Variabili, funzione prompt per input stringhe. <br> per a capo

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-window.console.log("ho fatto il c***o di caffè!!");
+window.console.log("ho fatto il c***o di caffè!!"); //come scrivere su standard output.(per JS è il log della console)
 document.write("Ho fatto il caffè");
 var Tazzina = 0;
 let Nuovatazzina = 0; // modo piú comune per inizializza una variabile: let= sia una variabile compresa fra.....
 const Chicco = 0; //da inizializzare poichè costante.
 let lattuga = 1;
-let cipolla = 0.989;
+let cipolla = 0.989; //float
 let banane = "gianni";
 let zucchero = true;
-
+const br = "<br>"; //la inizializzo come costante così potró inserirla come variabile per andare a capo poi
 if (cipolla > 2) {
   zucchero = false;
 } else {
   cipolla++; // cipolla = cipolla + 1
 }
 
-document.write(cipolla + "<br>"); //fa un cast fra un tipo (numero)ed un tipo (stringa"") facendo una concatenazione
+document.write(cipolla + br); //fa un cast fra un tipo (numero)ed un tipo (stringa"") facendo una concatenazione
 for (let i = 0; i <= 100; i++) {
   //dì dieci frasi- quantificato a priori,
   lattuga++;
@@ -26,6 +26,9 @@ while (lattuga < 101) {
   lattuga++;
 }
 
-document.write(cipolla + "<br>");
+document.write(cipolla + br);
 
 let aglio = 1 + 3;
+let melanzana = prompt("scrivi qui");
+
+document.write(melanzana + br);
